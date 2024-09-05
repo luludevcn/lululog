f69530318dc1af39866295279dd7ee96334ebe4a
luludev <523946878@qq.com>
2024-09-05 19:10:35 +0800

    learning steps of  astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,29 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import remarkToc from 'remark-toc';
+
+import rehypePresetMinify from 'rehype-preset-minify';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  site: 'https://luludevcn.github.io'
+  integrations: [react(), tailwind(), sitemap({
+    // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
+    // customPages: ['https://stargazers.club/external-page', 'https://stargazers.club/external-page2'],
+    changefreq: 'weekly',
+    priority: 0.9,
+    lastmod: new Date(),
+  }), mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'dracula' },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypePresetMinify],
+    remarkRehype: { footnoteLabel: 'Footnotes' },
+    gfm: false,
+  })],
+  site: 'https://luludev.cn'
 });

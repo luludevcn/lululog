7e8c390b67d712eb3aa75751756664d8be781471
luludev <523946878@qq.com>
2024-09-08 15:34:06 +0800

    update site

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,9 +3,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
   return rss({
     title: 'Luludev | Blog',
-    description: 'My journey learning Astro',
+    description: '一个程序员的开发日志',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md*')),
     customData: `<language>en-us</language>`,
   });
 }
